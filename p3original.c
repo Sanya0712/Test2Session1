ad19a1f35da92a24508639ea29849a07a154d069
sanya0712 <97089970+Sanya0712@users.noreply.github.com>
2022-02-23 04:52:50 +0000

    main3

---
+++ p3original.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int input_number()
+{
+  int num;
+  printf("enter the number:\n");
+  scanf("%d",&num);
+  return num;
+}
+
+int is_prime(int n)
+{
+  int i,c;
+  c=0;
+  i=1;
+  while(n>=i)
+  {
+    if (n%i==0)
+    {
+      c=c+1;
+    }
+    i++;   
+  }
+  return c;
+}  
+
+  void output(int n,int is_prime)
+  {
+    if (is_prime>2)
+    {
+      printf("%d is a composite number \n",n);
+    }
+    else
+    {
+      printf("%d is a prime number\n",n);
+    }
+  }
+
+  int main()
+  {
+    int num,result;
+    num=input_number();
+    result=is_prime(num);
+    output(num,result);
+    return 0;
+  }
